fb9bb1b7ace7ac2d0a902fd80f971ff63109983e
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-11-12 21:30:40 +0800

    feat: Add CORS headers and JSON response format; handle GET request with success response and 404 for undefined routes

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const http = require("http");
 
 const requestListener = (req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
+  const headers = {
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
+    "Content-Type": "application/json, charset=utf-8"
+  }
 
   if(req.url == "/" && req.method == "GET") {
     res.writeHead(200, headers);
-    res.write("Hello");
-    res.end();
-  }else if(req.url == "/" && req.method == "DELETE") {
-    res.writeHead(200, headers);
-    res.write("successfully deleted");
+    res.write(JSON.stringify({
+      "status": "success",
+      "data": []
+    }));
     res.end();
   }else{
     res.writeHead(404, headers);
-    res.write("not found 404");
+    res.write(JSON.stringify({
+      "status": "false",
+      "message": "無此網站路由"
+    }));
     res.end();
   }
 }
