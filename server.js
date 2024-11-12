13ba9f28328f0dfa1a05cb8b3b2d1993c7f866bb
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-11-12 21:23:23 +0800

    feat: Update server to handle GET and DELETE requests at root endpoint with appropriate responses

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const http = require("http");
 
 const requestListener = (req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Hello");
-  res.end();
+
+  if(req.url == "/" && req.method == "GET") {
+    res.writeHead(200, headers);
+    res.write("Hello");
+    res.end();
+  }else if(req.url == "/" && req.method == "DELETE") {
+    res.writeHead(200, headers);
+    res.write("successfully deleted");
+    res.end();
+  }else{
+    res.writeHead(404, headers);
+    res.write("not found 404");
+    res.end();
+  }
 }
 
 const server = http.createServer(requestListener);
