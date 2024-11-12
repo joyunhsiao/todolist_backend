bb5d977d67b394b8025e04b56b131c74899ec832
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-11-12 21:38:45 +0800

    feat: Add handling for OPTIONS preflight requests with CORS headers

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const requestListener = (req, res) => {
       "data": []
     }));
     res.end();
+  }else if(req.method == "OPTIONS") {
+    res.writeHead(200, headers);
+    res.end();
   }else{
     res.writeHead(404, headers);
     res.write(JSON.stringify({
