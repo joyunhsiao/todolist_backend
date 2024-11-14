4d0f5f97b0295f8ae0388ffae2ec0e88b5945ff6
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-11-14 11:49:07 +0800

    fix: Correct missing parentheses in PATCH route for updating todo title by ID

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const requestListener = (req, res) => {
     req.on("end", () => {
       try{
         const todo = JSON.parse(body).title;
-        const id = req.url.split("/").pop;
+        const id = req.url.split("/").pop();
         const index = todos.findIndex(element => element.id == id);
         if (todo !== undefined && index !== -1) {
           todos[index].title = todo;
